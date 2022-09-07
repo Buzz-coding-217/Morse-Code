dc615323e64609c77c6dccab20e93d34c9ecd7ef
Shravel <103954308+Shravel-Sharma@users.noreply.github.com>
2022-09-07 11:14:07 +0530

    Code for Morse code.

---
+++ Blink_Morse_Code.ino
@@ -0,0 +1,99 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function to blink the remorse code of 'shravel'
+void loop() {
+  //S
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(200);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(200);     
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(200); 
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(600);
+  //H
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(200);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(200); 
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(200);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(200); 
+  digitalWrite(LED_BUILTIN, HIGH);    
+  delay(200); 
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(600); 
+  //R
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(200);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(200); 
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(600);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(200); 
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(200);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(600); 
+  //A
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(200);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(200); 
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(600);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(600); 
+  //V
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(200);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(200);     
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(200);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(200); 
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(200);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(200); 
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(600);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(600); 
+  //E
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(200);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(600); 
+  //L
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(200);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(200);     
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(600);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(200); 
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(200);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(200); 
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(200);   
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1400); 
+}
